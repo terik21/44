3934ff64e8a03e99d1099e61f6c6105c2c5a53b1
terik21 <151690487+terik21@users.noreply.github.com>
2024-04-25 17:11:24 +0300

    Add files via upload

---
+++ 44/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace pc
+{
+    class Programm
+    {
+        static void Main()
+        {
+            int i = int.Parse(Console.ReadLine());
+            table(i);
+        }
+        static void table(int x)
+        {
+            Console.Write("  ");
+            int a = 1;
+            while (a <= x)
+            {
+                Console.Write($"{a} ");
+                a++;
+            }
+            Console.WriteLine();
+            a = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                Console.Write($"{i} ");
+                for (int j = 1; j <= x; j++)
+                {
+                    Console.Write($"{i*j} ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
